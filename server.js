1387eef05a3b36c9caf0f707bb5a52861e59464b
Connor Findlay <connor.findlay@gmail.com>
2020-04-03 13:09:45 +1300

    Beginning to set up for certificate authentication on specified routes.

--- server.js
+++ server.js
@@ -19,14 +19,13 @@ function start(route, handle) {
 		key: fs.readFileSync(`/etc/letsencrypt/live/${certdir}/privkey.pem`),
 		cert: fs.readFileSync(`/etc/letsencrypt/live/${certdir}/fullchain.pem`), 
 		dhparam: fs.readFileSync(`/etc/letsencrypt/live/${certdir}/dh-strong.pem`),
-		ciphers: ["TLS_AES_256_GCM_SHA384","TLS_CHACHA20_POLY1305_SHA256",
-				"TLS_AES_128_GCM_SHA256","TLS_AES_128_CCM_SHA256",
-				"TLS_AES_128_CCM_8_SHA256","ECDHE-ECDSA-AES256-GCM-SHA384",
-				"ECDHE-RSA-AES256-GCM-SHA384","ECDHE-ECDSA-CHACHA20-POLY1305",
-				"ECDHE-RSA-CHACHA20-POLY1305","ECDHE-ECDSA-AES128-GCM-SHA256",
-				"ECDHE-RSA-AES128-GCM-SHA256","ESDHE-ECDSA-AES256-SHA384",
-				"ECDHE-ECDSA-AES128-SHA256","ECHED-RSA-AES128-SHA256"].join(':'),
-		honorCipherOrder: true
+		ciphers: ["TLS_AES_256_GCM_SHA384","TLS_CHACHA20_POLY1305_SHA256", "TLS_AES_128_GCM_SHA256","TLS_AES_128_CCM_SHA256", "TLS_AES_128_CCM_8_SHA256","ECDHE-ECDSA-AES256-GCM-SHA384", "ECDHE-RSA-AES256-GCM-SHA384","ECDHE-ECDSA-CHACHA20-POLY1305", "ECDHE-RSA-CHACHA20-POLY1305","ECDHE-ECDSA-AES128-GCM-SHA256", "ECDHE-RSA-AES128-GCM-SHA256","ESDHE-ECDSA-AES256-SHA384", "ECDHE-ECDSA-AES128-SHA256","ECHED-RSA-AES128-SHA256"].join(':'),
+		honorCipherOrder: true,
+		requestCert: true,
+		rejectUnauthorized: false, //Perform our own error handling
+		ca: [
+			fs.readFileSync(`/etc/letsencrypt/live/${certdir}/server-cert.pem`)
+		]
 	}, onRequest).listen(443);
 	console.log("Server started.");
 	
